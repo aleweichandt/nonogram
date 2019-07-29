89ff3591c140384177e4bad456f43c3e100952f9
Alejandro Weichandt <aleweichandt@gmail.com>
2019-07-29 22:30:20 +0200

    fix sagas

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,12 +1,14 @@
+// @flow
+import type { Saga } from 'redux-saga';
 import { all } from 'redux-saga/effects';
 
 import { sagas as navigationSagas } from '../navigation';
 import { sagas as gameSagas } from '../game';
 
-export default function* rootSaga() {
+export default function* rootSaga(): Saga<void> {
   const sagas = [
     ...navigationSagas,
-    gameSagas,
+    ...gameSagas,
   ].map(saga => saga());
   yield all(sagas);
 }
