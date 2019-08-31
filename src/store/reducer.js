cd59af6cbcb95f61efbc109c39fc57678c30adf2
Alejandro Weichandt <aleweichandt@gmail.com>
2019-08-31 10:30:23 +0200

    integrate game selection module into app

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as navigation } from '../navigation';
 import { reducer as game } from '../game';
+import { reducer as gameSelection } from '../game-selection';
 
 export default combineReducers({
   ...navigation,
   ...game,
+  ...gameSelection,
 });
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -4,11 +4,13 @@ import { all, fork } from 'redux-saga/effects';
 
 import { sagas as navigationSagas } from '../navigation';
 import { sagas as gameSagas } from '../game';
+import { sagas as gameSelectionSagas } from '../game-selection';
 
 export default function* rootSaga(): Saga<void> {
   const sagas = [
     ...navigationSagas,
     ...gameSagas,
+    ...gameSelectionSagas,
   ].map(saga => fork(saga));
   yield all(sagas);
 }
