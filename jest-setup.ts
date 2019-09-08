7bb57e586764a7c294c9f1cb7b0c43399000daaa
Alejandro Weichandt <aleweichandt@gmail.com>
2019-09-08 13:29:29 +0200

    restore missing config files

---
+++ jest-setup.ts
@@ -0,0 +1,12 @@
+import { Store } from 'redux';
+// @ts-ignore
+import mocks from 'react-native-jest-mocks';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+mocks.initAll();
+// Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('redux-logger', () => ({
+  createLogger: () => (store: Store) => (next: any) => (action: any) => next(action), 
+}));
