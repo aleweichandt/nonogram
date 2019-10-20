bf9972605fd2f5ec9b437e5bd7c865c26022858a
Alejandro Weichandt <aleweichandt@gmail.com>
2019-10-20 20:47:56 +0200

    remove redux logger

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -1,13 +1,8 @@
 import {applyMiddleware} from 'redux';
 import createSagaMiddleware, {Saga} from 'redux-saga';
-import {createLogger} from 'redux-logger';
 
 const sagaMiddleware = createSagaMiddleware();
-const logger = createLogger({});
-
-// eslint-disable-next-line
-const devMiddleware = __DEV__ ? [logger] : [];
 
 export const runSaga = (sagas: Saga<any[]>) => sagaMiddleware.run(sagas);
 
-export default applyMiddleware(sagaMiddleware, ...devMiddleware);
+export default applyMiddleware(sagaMiddleware);
